d642afc60cffcb0bb2c5491d214862586f046f1d
Mario <mario.buonomo@facile.it>
2020-08-11 16:07:22 +0200

    ref: removed prisma cli

--- makefiles/prisma.mk
+++ makefiles/prisma.mk
@@ -1,8 +1,5 @@
 schemaPath = ./prisma/schema/schema.prisma
 
-# prisma_install_cli:
-# 	${docker} ${container} npm install @prisma/cli --save-dev
-
 prisma_generate:
 	${docker} ${container} ./node_modules/.bin/prisma generate --schema ${schemaPath}
 

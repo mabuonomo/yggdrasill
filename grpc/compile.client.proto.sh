a1d92a58846afec7b0541238bc81c11953897241
Mario <mario.buonomo@facile.it>
2020-08-12 12:01:46 +0200

    grpc refactor

--- grpc/compile.client.proto.sh
+++ grpc/compile.client.proto.sh
@@ -1,5 +1,10 @@
+SCHEMA_PATH=/main/schema/grpc
+
+rm -R $SCHEMA_PATH/grpc/dist/* || true
+mkdir $SCHEMA_PATH/grpc/dist || true
+
 # nestjs
-protoc \
+protoc -I $SCHEMA_PATH \
 --plugin=./node_modules/.bin/protoc-gen-ts_proto \
---ts_proto_out=./grpc/build/nestjs ./grpc/proto/*.proto \
+--ts_proto_out=$SCHEMA_PATH/dist $SCHEMA_PATH/*.proto \
 --ts_proto_opt=outputEncodeMethods=false,outputJsonMethods=false,outputClientImpl=false,nestJs=true,addGrpcMetadata=true
--- makefiles/grpc.mk
+++ makefiles/grpc.mk
@@ -1,10 +1,5 @@
-basePath = ./grpc
-
-__proto_build_client:
-	@mkdir ${basePath}/build || true
-	@rm -R ${basePath}/build/nestjs/* || true
-	@mkdir ${basePath}/build/nestjs || true
-	@${docker} ${container} sh ${basePath}/compile.client.proto.sh
+__proto_build_client: npm_init
+	@${docker} ${container} sh ./grpc/compile.client.proto.sh
 
 #build for web/nestjs
 grpc_build: __proto_build_client
